badd053a958f4cfd6c26395002d08a830edd87dd
Richard Duivenvoorde <richard@duif.net>
2020-10-29 16:24:34 +0100

    First commit

---
+++ src/app/__init__.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+from blikveld.blikveld import BlikVeld
+#from blikveld import BlikVeld
+
+app = Flask(__name__)
+
+
+#Thinking about api's here:
+# /         html page with entrypoints/examples
+# /camera   GET input form POST result html page
+# /api      GET help page (OR also working) POST retrieve geojson (of camera, all panden and blikveld panden)
+# ??
+
+
+@app.route('/')
+def run():
+    return 'ok'
+
+@app.route('/blikveld')
+def blikveld():
+    return BlikVeld().run()
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/camera', methods=['POST', 'GET'])
+def camera():
+    if request.method == 'POST':
+        camera_json = request.form['camera_json']
+        result_json = BlikVeld().run(camera_json)
+        #return result_json
+        #return render_template('result.html', result_json=result_json)
+        import urllib.parse
+        result_json_encoded = urllib.parse.quote(result_json)
+        return render_template('result.html', result_json=result_json, result_json_encoded=result_json_encoded)
+    else:
+        return render_template('camera.html')
---
+++ src/blikveld/blikveld.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+
+# pip install geojson
+import geojson  # https://pypi.org/project/geojson/
+# pip install requests
+import requests
+
+# Camera in Atjestraat:
+# kijkhoek/sector 39 graden
+# bearing: 31 graden
+CAMERA_JSON = """
+{
+  "type": "Feature",
+  "properties": {
+    "angle": 38.876645504061116,
+    "bearing": 31.979981482645943,
+    "distance": 87.9921646359149
+  },
+  "geometry": {
+    "type": "GeometryCollection",
+    "geometries": [
+      {
+        "type": "Point",
+        "coordinates": [
+          4.646878838539124,
+          52.3969167956431
+        ]
+      },
+      {
+        "type": "LineString",
+        "coordinates": [
+          [
+            4.647177386477727,
+            52.39773567907421
+          ],
+          [
+            4.647953579015309,
+            52.39743995473123
+          ]
+        ]
+      }
+    ]
+  }
+}
+"""
+
+class BlikVeldException(BaseException):
+    pass
+
+class BlikVeld:
+
+
+    #def main():
+    def run(self, camera_json=None):
+
+        if not camera_json:
+            camera_json = CAMERA_JSON
+
+        # input is a camera json
+        camera = geojson.loads(camera_json)
+        #print(camera)
+
+        if not ('geometry' in camera and 'geometries' in camera['geometry']):
+            raise BlikVeldException('Camera json should be valid geojson, and have a "geometry" with two "geometries" ')
+            return
+
+        view_point = None
+        horizon = None
+        for geom in camera['geometry']['geometries']:
+            gtype = geom['type'].lower()
+            if gtype == 'point':
+                view_point = geom
+            elif gtype == 'linestring':
+                horizon = geom
+            else:
+                raise BlikVeldException('Unknown geometry type in camera json')
+
+        # {"coordinates": [4.646879, 52.396917], "type": "Point"} {"coordinates": [[4.647177, 52.397736], [4.647954, 52.39744]], "type": "LineString"}
+        #print(view_point['coordinates'], horizon['coordinates'])
+        # create one 3-point LineString from both geometries
+        coords = list(horizon['coordinates'])  # create a copy to not update the original
+        coords.insert(0, view_point['coordinates'])
+        coords.append(view_point['coordinates'])
+        # now create a proper geojson.Feature from it
+        polygon = geojson.Polygon([coords])
+        camera_feature = geojson.Feature(geometry=polygon, properties={'name': 'camera'})
+        #fc = geojson.FeatureCollection([camera_feature])
+        #print(geojson.dumps(fc))
+
+        #print(geojson.dumps(camera_feature))
+        # some magic to create the posList we use in the gml
+        # WFS2: Y X instead of X Y !!
+        poslist = ' '.join(f'{i[1]} {i[0]}' for i in coords)
+        print(poslist)
+
+        # ARGH!!!! owslib does not do spatial filtering !!! https://github.com/geopython/OWSLib/issues/128
+        # we COULD do with bbox, but ...
+        # from owslib.wfs import WebFeatureService
+        # from owslib.etree import etree
+        # from owslib.fes import Prop
+        # pdok_wfs = WebFeatureService(url='https://geodata.nationaalgeoregister.nl/kadastralekaart/wfs/v4_0', version='2.0.0')
+        # filter = PropertyIsLike(propertyname='bez_gem', literal='Ingolstadt', wildCard='*')
+        # response = pdok_wfs.getfeature(typename='kadastralekaartv4:bebouwing', filter=)
+
+        # BAG WFS
+        wfs_url = 'https://geodata.nationaalgeoregister.nl/bag/wfs/v1_1?'
+        wfs_type = 'bag:pand'
+        wfs_timout = 1.5  # bag endpoint is fast!!
+
+        # kadastrale kaart WFS
+        # wfs_url = 'https://geodata.nationaalgeoregister.nl/kadastralekaart/wfs/v4_0?'
+        # wfs_type = 'kadastralekaartv4:bebouwing'
+        # wfs_timout = 5  # kadastralekaart endpoint is NOT fast!!
+
+        #spatial_filter = f'<Filter><Intersects> <PropertyName>Geometry</PropertyName><gml:Polygon srsName="urn:ogc:def:crs:EPSG::4326"><gml:exterior><gml:LinearRing><gml:posList>52.397328 4.647181 52.397753 4.648073 52.397237 4.648182 52.397328 4.647181</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon></Intersects></Filter>'
+        spatial_filter = f'<Filter><Intersects>' \
+                         f'<PropertyName>Geometry</PropertyName>' \
+                         f'<gml:Polygon srsName="urn:ogc:def:crs:EPSG::4326">' \
+                         f'<gml:exterior><gml:LinearRing><gml:posList>' \
+                         f'{poslist}' \
+                         f'</gml:posList></gml:LinearRing></gml:exterior>' \
+                         f'</gml:Polygon></Intersects></Filter>'
+        params = {
+            'service': 'WFS',
+            'request': 'GetFeature',
+            'version': '2.0.0',
+            'srsName': 'urn:ogc:def:crs:EPSG::4326',
+            'outputFormat': 'application/json; subtype=geojson',  # service is VERY strict: 'application/json;subtype=geojson' (no space in between type;subtype) raises 400
+            'typeNames': wfs_type,
+            'filter': spatial_filter
+        }
+
+        DEV = False
+
+        if DEV:
+            with open('/tmp/panden.json', 'r') as f:
+                panden = geojson.load(f)
+        else:
+            r = requests.get(wfs_url, params=params, timeout=wfs_timout)
+            if 200 != r.status_code:
+                raise BlikVeldException(f'WFS url returned status {r.status_code}:\n{r.url}')
+            print(r.url)
+            panden = geojson.loads(r.text)
+            panden.features.insert(0, camera_feature)
+            with open('/tmp/panden.json', 'w') as f:
+                geojson.dump(panden, f, indent=2)
+            print(f'Found {len(panden["features"])} panden with camera json')
+
+
+        # 2 scenario's:
+        # -1- for every pand/polygon get the coordinates, create a line from viewpoint to coordinate, and create a list of polygons which do NOT cross another polygon
+        # -2- within the 'view triangle' create 'beams' (per 1 (0.5) degree) and see which polygon crosses and add the nearest to viewpoint to the list
+
+        # so panden is a (geojson)FeatureCollection object (! not a dict)
+        import shapely.geometry
+
+        view_point = shapely.geometry.shape(view_point)
+
+        # first create a dict of feature_identiefication -> shapely polygons
+        shape_dict = {}
+        for feature in panden.features:
+            if not 'bouwjaar' in feature.properties:
+                continue
+            shape_dict[feature.properties['gid']] = shapely.geometry.shape(feature.geometry)
+        i=0
+        result = []
+        from shapely.prepared import prep
+        for pand_id, pand_geom in shape_dict.items():
+            # if i == 4:
+            #     break
+            # i += 1
+            print(pand_geom)
+            for vertex in pand_geom.exterior.coords:
+                #print(vertex, view_point.coords[0])
+                line = shapely.geometry.LineString([vertex, view_point.coords[0]])
+
+                prepared_line = prep(line)
+                hits = list(filter(prepared_line.crosses, shape_dict.values()))
+                #print(len(hits))
+                if len(hits) == 0:
+                    # this means that THIS line of sight of:
+                    # - viewpoint TO vertex
+                    # did NOT cross another pand, meaning:
+                    # the pand with this vertex is probably in sight from the viewpoint
+                    print(f'Adding {pand_id} to the results')
+                    centroid = pand_geom.centroid  # returns a geojson.Point
+                    feature = geojson.feature.Feature(geometry=centroid, properties={'id': pand_id})
+                    result.append(feature)
+                    break
+
+        for feature in result:
+            panden.features.insert(0, feature)
+        with open('/tmp/pandenresult.json', 'w') as f:
+            geojson.dump(panden, f, indent=2)
+
+        return geojson.dumps(panden)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    BlikVeld().run()
